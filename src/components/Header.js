3732990f5498ffd6159524f676fdd12f7fdeefaf
lim-youn-ho <limyounhoo@gmail.com>
2024-03-16 18:26:09 +0900

    css 라이브러리 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 
-import "@Components/Header"
+import "@Styles/Header.css"
 import logo from "@Styles/shareHouseLogo.jpg";
 import {Link,useNavigate} from "react-router-dom";
+import {AppBar,Container,Button,Toolbar ,Box } from '@mui/material';
+
 function Header(){
     const navigate  = useNavigate();
     function handleLogoClick() {
@@ -9,9 +11,22 @@ function Header(){
     }
 
     return(
-    <Link to="/" className="logo" onClick={handleLogoClick}>
-        <img src={logo} alt="Logo" position='left' style={{ marginRight: '1.5em', height:'5rem' }} />
-    </Link>
+        <AppBar className="AppBar"  position="static"
+                sx={{
+                    backgroundColor: 'black',
+                }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Link to="/" className="logo" onClick={handleLogoClick}>
+                        <img src={logo} alt="Logo" position='left' style={{ marginRight: '1.5em'}} />
+                    </Link>
+                    <Box horizontal={ 'right'}>
+                        <Button variant="outlined"  size="small">로그인</Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+
     );
 
 }

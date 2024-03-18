036d30696d18e3f0e03b3d7b72d3a4f7547d209a
lim-youn-ho <limyounhoo@gmail.com>
2024-03-18 17:41:07 +0900

    메뉴 바 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,34 @@
 import "@Styles/Header.css"
 import logo from "@Styles/shareHouseLogo.jpg";
 import {Link,useNavigate} from "react-router-dom";
-import {AppBar,Container,Button,Toolbar ,Box } from '@mui/material';
+import {
+    AppBar,
+    Container,
+    Button,
+    Toolbar,
+    Box,
+    Tooltip,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText, Collapse, List
+} from '@mui/material';
+import {ExpandLess ,ExpandMore ,StarBorder  }from '@mui/icons-material';
+import {useState} from "react";
+
 
 function Header(){
     const navigate  = useNavigate();
+
     function handleLogoClick() {
         navigate("/");
     }
+
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
 
     return(
         <AppBar className="AppBar"  position="static"
@@ -16,14 +37,30 @@ function Header(){
                     backgroundColor: 'black',
                 }}>
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
+
                     <Link to="/" className="logo" onClick={handleLogoClick}>
                         <img src={logo} alt="Logo" position='left' style={{ marginRight: '1.5em'}} />
                     </Link>
-                    <Box horizontal={ 'right'}>
-                        <Button variant="outlined"  size="small">로그인</Button>
-                    </Box>
-                </Toolbar>
+                    <div  style={{float: 'right'}}>
+                        <ListItemButton onClick={handleClick}>
+                            <ListItemIcon>
+
+                            </ListItemIcon>
+                            <ListItemText primary="menu" />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </ListItemButton>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        <StarBorder />
+                                    </ListItemIcon>
+                                    <ListItemText primary="로그인" />
+                                </ListItemButton>
+                            </List>
+                        </Collapse>
+                    </div>
+
             </Container>
         </AppBar>
 

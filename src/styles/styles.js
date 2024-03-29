39b76c48ccb5a36c45be16aef09135e8fd77aa42
lim-youn-ho <limyounhoo@gmail.com>
2024-03-29 17:51:23 +0900

    css관리용

---
+++ src/styles/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+    header: {
+        backgroundColor: "#EEE",
+    },
+    pricing: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "baseline",
+        marginBottom: "16px",
+    },
+    descriptions: {
+        margin: 0,
+        padding: 0,
+        listStyle: "none",
+    },
+});
